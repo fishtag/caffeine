d69e075d3eb9b62498dea9a302e2385b31a2d7c8
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-02-05 11:14:38 +0300

    Add Album and link it with Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   # include Sluggable
 
-  # belongs_to :album
+  has_many :albums
   # has_many :user_forms
   # has_many :page_attachments
   # has_one :page_image
