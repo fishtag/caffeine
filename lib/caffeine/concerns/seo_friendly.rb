48135063645d7c82931184f76b6dad4b091b28b5
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-03-13 17:55:24 +0300

    Permit SeoDatum attributes for Page

--- lib/caffeine/concerns/seo_friendly.rb
+++ lib/caffeine/concerns/seo_friendly.rb
@@ -8,6 +8,8 @@ module Caffeine
           acts_as_seo_friendly
 
           after_initialize :build_seo_datum, if: ->(obj) { obj.seo_datum.blank? }
+
+          self.permitted_attributes << { seo_datum_attributes: ::ActsAsSeoFriendly::SeoDatum::PERMITTED_ATTRIBUTES }
         end
       end
     end
