3b55a62488a172a4e6179e0f6e8cef69e2efee1c
Emil Shakirov <5o.smoker@gmail.com>
2015-06-10 13:46:44 +0300

    add picture description to permit params

--- app/models/caffeine/picture.rb
+++ app/models/caffeine/picture.rb
@@ -1,6 +1,6 @@
 module Caffeine
   class Picture < ActiveRecord::Base
-    PERMITTED_ATTRIBUTES = %i(id image title caption position _destroy)
+    PERMITTED_ATTRIBUTES = %i(id image title caption position description _destroy)
 
     belongs_to :imageable, polymorphic: true
 
