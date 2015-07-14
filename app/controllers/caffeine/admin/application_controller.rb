6271c957537a183daf423716f6ea445211536d66
Emil Shakirov <emil.shakirov@fishtag.ru>
2015-07-14 13:41:10 +0300

    WIP

--- app/controllers/caffeine/admin/application_controller.rb
+++ app/controllers/caffeine/admin/application_controller.rb
@@ -2,6 +2,7 @@ module Caffeine
   module Admin
     class ApplicationController < CaffeineController
       expose(:pages) { Caffeine::Page.roots_and_descendants_preordered.decorate }
+      expose(:root_pages) { Caffeine::Page.roots }
 
       helper Caffeine::Engine.helpers
 
--- app/helpers/caffeine/application_helper.rb
+++ app/helpers/caffeine/application_helper.rb
@@ -1,4 +1,45 @@
 module Caffeine
   module ApplicationHelper
+    def draw_tree(child)
+      render partial: 'caffeine/admin/partials/modules/item', locals: {page: child}
+      if child.children.any?
+        draw_tree(child)
+      end
+    end
+
+    def draw_childs_tree(children)
+      concat (children.collect do |child|
+         content_tag(:span, class: 'nav__item--fill') {
+                 link_to '#', class: 'title' do
+                   content_tag(:i, class: 'fa fa-file-text-o') {
+                     child.title
+                     if child.children.present?
+                       content_for(:span, class: 'count-children') { child.children.count }
+                     end
+                   }
+                 end
+                 if child.children.present?
+                   content_tag(:span, class: 'more-children action') {
+                     content_tag(:span, class: 'action__icon') {
+                       content_tag(:i, class: 'iconsfont iconsfont-plus')
+                     }
+                   }
+                 end
+               }
+                return if child.children.blank?
+                draw_childs_tree(child.children)
+             end
+         ).join("\n")
+    end
+
+    def draw_descendants(children)
+      html = concat (children.collect do |child|
+               render partial: 'caffeine/admin/partials/modules/item', locals: {page: child}
+               # return if child.children.blank?
+               # draw_descendants(child.children)
+             end
+       ).join("\n")
+      html
+    end
   end
 end
