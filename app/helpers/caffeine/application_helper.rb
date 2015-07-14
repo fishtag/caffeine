06caa605fdd0a8ff750c10dc2209449c6249c997
Daniil Klementev <daniil.klementyev@fishtag.ru>
2015-07-14 18:51:03 +0300

    Moved nav_pages main partials

--- app/helpers/caffeine/application_helper.rb
+++ app/helpers/caffeine/application_helper.rb
@@ -2,7 +2,7 @@ module Caffeine
   module ApplicationHelper
     def draw_descendants(children)
       children.collect { |page|
-        concat render 'caffeine/admin/partials/modules/item', page: page
+        concat render 'caffeine/admin/partials/modules/nav_pages/item', page: page
         if page.children.any?
            content_tag(:ul, class: 'nav__list nav__list--sub') {
                  draw_descendants(page.children)
