353ab54a15c86a022c66e4dce5c30ffee0c3d23e
Emil Shakirov <emil.shakirov@fishtag.ru>
2015-07-20 11:24:55 +0300

    Add config for custom app name

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -26,6 +26,7 @@ module Dummy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.custom_app_name = 'Дамми'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
