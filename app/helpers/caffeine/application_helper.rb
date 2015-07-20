4c1cbe9d89a8c85aae97aad279156ae08b33f795
Emil Shakirov <emil.shakirov@fishtag.ru>
2015-07-20 13:21:24 +0300

    Fix favicon_path

--- app/helpers/caffeine/application_helper.rb
+++ app/helpers/caffeine/application_helper.rb
@@ -2,12 +2,13 @@ module Caffeine
   module ApplicationHelper
     def favicon_path
       path = has_asset?('favicon') ? 'favicon' : 'caffeine/admin/icons/favicon'
-      image_path(path)
+      image_url(path)
     end
 
     def has_asset?(path)
       Rails.application.assets.find_asset(path).present?
     end
+
     def draw_descendants(children)
       children.collect { |page|
         concat render 'caffeine/admin/partials/modules/nav_pages/item', page: page
