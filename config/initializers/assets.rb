c7691c73185bdd85560590d09fd9eb529ec226ac
Emil Shakirov <emil.shakirov@fishtag.ru>
2015-08-14 15:22:37 +0300

    Fix iconfonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
-Caffeine::Engine.config.assets.paths << "#{Rails.root}/app/assets/fonts"
-Caffeine::Engine.config.assets.precompile += %w(iconsfont.eot iconsfont.svg iconsfont.ttf iconsfont.woff)
 Caffeine::Engine.config.assets.precompile += %w(caffeine/admin.js caffeine/admin.css)
 Caffeine::Engine.config.assets.precompile += %w(caffeine/sessions.css)
 Caffeine::Engine.config.assets.precompile += %w(bootstrap/bootstrap.min.css bootstrap/bootstrap.min.js)
